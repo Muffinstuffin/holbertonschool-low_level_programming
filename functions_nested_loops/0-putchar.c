ddfeca986e945f79ec515a5f404fda1189a6a982
Tanita de Lange <tanitadelange@gmail.com>
2024-06-19 13:23:13 -0400

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
@@ -18,15 +17,4 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-=======
-#include <unistd.h>
-
-int main(void)
-{
-    char text[] = "_putchar\n";
-    for (int i = 0; text[i] != '\0'; i++) {
-        write(1, &text[i], 1);
-    }
-    return 0;
->>>>>>> 6ae4d66d5a09b3dc68c2360df6de15a56d1e9a57
 }
